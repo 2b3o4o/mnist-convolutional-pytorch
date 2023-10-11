7e51e778b6b1e149e4e61f4b2409d80520ee60a7
2b3o4o <3944433+2b3o4o@users.noreply.github.com>
2023-10-11 01:18:09 -0700

    fixed a mistake I made during refactoring which caused performance to degrade

--- src/model.py
+++ src/model.py
@@ -1,12 +1,13 @@
 import torch.nn as nn
 
 class ConvLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, dropout_rate):
+    def __init__(self, in_channels, out_channels, dropout_rate, use_stride=True):
         super().__init__()
 
         modules = [nn.Dropout(p=dropout_rate)] if dropout_rate > 0 else []
         modules.extend([
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=2, padding=1),
+            (nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=2, padding=1) if use_stride
+                else nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3)),
             nn.ReLU()
         ])
         self.layers = nn.ModuleList(modules)
@@ -39,7 +40,7 @@ class ConvNet(nn.Module):
         dropout_rate = 0.5
 
         self.conv = nn.ModuleList([
-            ConvLayer(in_channels=1, out_channels=16, dropout_rate=0),
+            ConvLayer(in_channels=1, out_channels=16, dropout_rate=0, use_stride=False),
             ConvLayer(in_channels=16, out_channels=64, dropout_rate=dropout_rate),
             ConvLayer(in_channels=64, out_channels=64, dropout_rate=dropout_rate)
         ])
@@ -47,7 +48,7 @@ class ConvNet(nn.Module):
         self.flatten = nn.Flatten()
 
         self.linear = nn.ModuleList([
-            LinearLayer(in_channels=1024, out_channels=hidden_size, dropout_rate=dropout_rate, relu=True),
+            LinearLayer(in_channels=(26 // 2 // 2 + 1) ** 2 * 64, out_channels=hidden_size, dropout_rate=dropout_rate, relu=True),
             LinearLayer(in_channels=hidden_size, out_channels=hidden_size, dropout_rate=dropout_rate, relu=True),
             LinearLayer(in_channels=hidden_size, out_channels=output_size, dropout_rate=dropout_rate, relu=False)
         ])
